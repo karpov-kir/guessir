60b4f91db511d031e6464fb187e5cd7948e7ee00
Kirill Karpov <karpov.kir.v@gmail.com>
2022-03-29 23:12:32 +0300

    Add GH actions integration

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,5 @@ const baseConfig = require('./jest.config.base');
 
 module.exports = {
   ...baseConfig,
-  roots: ['<rootDir>'],
   projects: ['<rootDir>/packages/backend', '<rootDir>/packages/web'],
 };
