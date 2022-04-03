a8971c79a9e56f0aa89b08bcf640867eef041897
Kirill Karpov <karpov.kir.v@gmail.com>
2022-04-03 22:09:35 +0300

    Fix a code smell

--- packages/web/src/testUtils/dom.ts
+++ packages/web/src/testUtils/dom.ts
@@ -1,4 +1,3 @@
 export function getElementText(container: HTMLElement, selector: string) {
-  const element = container.querySelector(selector) as HTMLElement | null;
-  return element ? element.textContent?.trim() : undefined;
+  return container.querySelector(selector)?.textContent?.trim();
 }
