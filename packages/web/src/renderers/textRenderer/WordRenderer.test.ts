afe45b1e0f29d18164104e5f2f2ececd9750192b
Kirill Karpov <karpov.kir.v@gmail.com>
2022-04-10 17:06:50 +0300

    Cover WordRenderer with unit tests

---
+++ packages/web/src/renderers/textRenderer/WordRenderer.test.ts
@@ -0,0 +1,40 @@
+import { Lexeme } from '../../lexemeAnalyzer';
+import { WordRenderer } from './WordRenderer';
+
+let wordRenderer: WordRenderer;
+const mockedLexeme = {
+  normalized: 'Test',
+} as Lexeme;
+const getLexemeElement = () => wordRenderer.getElement().querySelector('.lexeme') as HTMLElement;
+const getLexemeFirstLetterElement = () =>
+  wordRenderer.getElement().querySelector('.lexeme-first-letter') as HTMLElement;
+
+describe(WordRenderer, () => {
+  beforeEach(() => {
+    wordRenderer = new WordRenderer(mockedLexeme);
+  });
+
+  it('should render the word', () => {
+    expect(getLexemeElement().textContent).toBe('Test');
+    expect(getLexemeFirstLetterElement().textContent).toBe('T');
+    // Must be rendered hidden initially
+    expect(getLexemeElement().classList).not.toContain('show');
+  });
+
+  it('should show the word on click', () => {
+    wordRenderer.getElement().click();
+
+    expect(wordRenderer.getElement().classList).toContain('show');
+  });
+
+  it('should not notify about about second click', () => {
+    const userWordShowCallback = jest.fn();
+
+    wordRenderer.userWordShowEvent.subscribe(userWordShowCallback);
+    wordRenderer.getElement().click();
+    wordRenderer.getElement().click();
+
+    expect(userWordShowCallback).toBeCalledTimes(1);
+    expect(userWordShowCallback).toBeCalledWith(mockedLexeme);
+  });
+});
