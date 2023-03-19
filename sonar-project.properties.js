6206a7ddc28af5ac510572cf3ebac9269b94c1b5
Kirill Karpov <85180621+karpov-kir@users.noreply.github.com>
2023-03-19 16:40:01 +0400

    build: migrate CI to shared SonarQube action (#3)

---
+++ sonar-project.properties.js
@@ -0,0 +1,21 @@
+module.exports = {
+  'sonar.projectKey': 'Guessir',
+
+  'sonar.javascript.lcov.reportPaths': 'coverage/lcov.info',
+  'sonar.testExecutionReportPaths': 'coverage/test-report.xml',
+
+  'sonar.sources': ['packages/backend/src', 'packages/shared/src', 'packages/web/src'].join(','),
+  // SonarQube supports limited wildcards (https://docs.sonarqube.org/latest/project-administration/narrowing-the-focus).
+  // Exclude test files from analysis.
+  'sonar.exclusions': ['**/*.test.*', '**/*.spec.*', '**/*.e2e-spec.*'].join(','),
+
+  'sonar.coverage.exclusions': [
+    'packages/backend/src/migrations/*',
+    'packages/backend/src/dbUtils.ts',
+    'packages/backend/src/server.ts',
+    'packages/backend/src/MainModule.ts',
+    'packages/backend/src/index.ts',
+    'packages/web/src/testUtils/*',
+    'packages/web/src/index.ts',
+  ].join(','),
+};
