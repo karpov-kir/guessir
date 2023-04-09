8eb949e3017e6e1026a455a2606408279922b245
Kirill Karpov <karpov.kir.v@gmail.com>
2023-04-09 19:10:23 +0400

    fix: copy text URL button appearance

--- packages/web/vite.config.js
+++ packages/web/vite.config.js
@@ -1,5 +1,22 @@
-import { defineConfig } from 'vite'
+import fs from 'fs';
+import svgInlineLoader from 'svg-inline-loader';
+import { defineConfig } from 'vite';
+
+//TODO: remove this once https://github.com/vitejs/vite/pull/2909 gets merged
+export const svg = () => {
+  return {
+    name: 'vite-svg-patch-plugin',
+    transform: function (code, id) {
+      if (id.endsWith('.svg')) {
+        const extractedSvg = fs.readFileSync(id, 'utf8');
+        return `export default '${svgInlineLoader.getExtractedSVG(extractedSvg)}'`;
+      }
+      return code;
+    },
+  };
+};
 
 export default defineConfig({
   envPrefix: 'GUESSIR_',
-})
+  plugins: [svg()],
+});
